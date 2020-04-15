cecff58443298a2c5abc8306fa4d0fb9394db830
tanzeel152 <tanzeel152@gmail.com>
2020-04-15 22:24:52 +0500

    new project demo of property app

---
+++ db/migrate/20200414152752_create_properties.rb
@@ -0,0 +1,13 @@
+class CreateProperties < ActiveRecord::Migration[6.0]
+  def change
+    create_table :properties do |t|
+      t.string :name
+      t.string :address
+      t.integer :price
+      t.integer :rooms
+      t.integer :bathrooms
+
+      t.timestamps
+    end
+  end
+end
